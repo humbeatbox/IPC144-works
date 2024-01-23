efe95c79d9c406b2c862ec36048800f7aa174d05
humbeatbox <hchang67@seneca.ca>
2024-01-23 16:12:48 -0500

    diy2 haha

---
+++ workshops/diy2/diy2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  diy2
+//
+//  Created by Gary Chang on 2024-01-23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
