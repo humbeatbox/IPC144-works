40a0568406e33293ac126dd9ebf149a8983e4981
humbeatbox <hchang67@seneca.ca>
2024-02-13 22:17:01 -0500

    fix error

--- workshops/diy3/report.c
+++ workshops/diy3/report.c
@@ -7,35 +7,30 @@
 #include <stdio.h>
 #include <string.h>
 void report(int numberOfStudents) {
-    printf("What are the marks out of?\n>");
+    printf("What are the marks out of?\n> ");
     int maxMark = 0;
-    while(maxMark == 0){
+    while (maxMark == 0) {
         maxMark = getMaxMark();
     }
 
     //creat an array and fill with 0
     int score[numberOfStudents];
-    memset(score,0,sizeof(score));
+    memset(score, 0, sizeof(score));
 
     //save the each mark into an array
     int i;
     for (i = 0; i < numberOfStudents; i++) {
 
-        while(score[i] ==0){
-            printf("%d- ",i+1);
+        while (score[i] == 0) {
+            printf("%d- ", i + 1);
             score[i] = getMark(maxMark);
-            if (score[i] != 0){
-                printf("Entered: %d%%, %c\n",score[i], grade(score[i]));
+            if (score[i] != 0) {
+                printf("Entered: %d%%, %c\n", score[i], grade(score[i]));
             }
-//            printf("Entered: %d%%, %c\n",score[i], grade(score[i]));
         }
     }
 
-    printf("Lowest mark entered: %d\n", arrMinNumber(score,numberOfStudents));
-    printf("Highest mark entered: %d\n", arrMaxNumber(score,numberOfStudents));
-    printf("Average mark entered: %.1lf%%\n", arrAvgNumber(score,numberOfStudents));
-/*    for (i = 0; i < numberOfStudents; i++) {
-//        score[i] = getMark(i);
-        printf("%d\t",score[i]);
-    }*/
+    printf("Lowest mark entered: %d\n", arrMinNumber(score, numberOfStudents));
+    printf("Highest mark entered: %d\n", arrMaxNumber(score, numberOfStudents));
+    printf("Average mark entered: %.1lf%%\n", arrAvgNumber(score, numberOfStudents));
 }
