5ffcba0ca0dfb880928a965beffc62d005daeeb7
humbeatbox <hchang67@seneca.ca>
2024-03-10 00:01:16 -0500

    Q6 upload

--- workshops/midterm/main.c
+++ workshops/midterm/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 void equal(int i, int j);
+double listSum(double increment, double start, double end);
 int main() {
     printf("Start\n");
     equal(1, 2);
     equal(2, 1);
     equal(3, 3);
     printf("End\n");
+
+    printf("-------------------------------------------------------\n");
+    double inc, star, end;
+    printf("Enter a value to start with\n>");
+    scanf("%lf",&star);
+
+    printf("Enter the end value:\n>");
+    scanf("%lf",&end);
+
+    printf("Enter the increment value\n>");
+    scanf("%lf",&inc);
+
+
+    double sum = listSum(inc, star, end);
+    printf("\n");
+    printf("Sum of the numbers listed: %.1lf",sum);
     return 0;
 }
 void equal(int i, int j) {
@@ -16,4 +33,23 @@ void equal(int i, int j) {
     }else{
         printf("%d isequal to %d\n",i ,j);
     }
+}
+
+double listSum(double increment, double start, double end){
+
+    double sum = 0.0;
+    double ret = 0.0;
+    sum = start;
+    ret = start;
+    while( sum < end){
+        if(sum < end){
+            printf("%.1lf",sum);
+        }
+        sum = sum + increment;
+        if(sum < end) {
+            printf("-");
+            ret = ret + sum;
+        }
+    }
+    return ret;
 }
